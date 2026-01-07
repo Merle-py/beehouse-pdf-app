0f33b5baa3567c4f98565f468f4f11f32c4d2fe7
Gabriel <gbernardo21@gmail.com>
2026-01-07 14:45:00 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -5,16 +5,14 @@ const nextConfig = {
         serverComponentsExternalPackages: ['pdfkit'],
     },
 
-    // Permite que o Bitrix carregue o site em um Iframe
     async headers() {
         return [
             {
                 source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" },
-                    // Esta linha é a mais importante:
+                    // Mantenha apenas o CSP
                     { key: "Content-Security-Policy", value: "frame-ancestors 'self' https://*.bitrix24.com https://*.bitrix24.com.br https://*.bitrix24.ru https://*.bitrix24.eu" },
-                    { key: "X-Frame-Options", value: "ALLOWALL" }
                 ],
             },
         ];
